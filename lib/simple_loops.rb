7c6c4d142c1ef2adb4e1ec363a00792c84ccf291
Zecharias Mengis <zmengis@gmail.com>
2021-02-10 04:48:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,11 @@ def loop_message_n_times(parameter, integer)
     counter += 1 
   end
 end
+
+def output_array(array)
+  counter = 0 
+  while counter < array.length do
+    puts array(counter)
+    counter += 1 
+  end
+end
