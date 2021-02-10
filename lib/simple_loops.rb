d417b341852bae0e30118a8b75a5f23d33ed3994
Zecharias Mengis <zmengis@gmail.com>
2021-02-10 04:49:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,8 +16,8 @@ end
 
 def output_array(array)
   counter = 0 
-  while counter <= array.length do
-    puts array(counter)
+  while counter < array.length do
+    puts array[counter]
     counter += 1 
   end
 end
